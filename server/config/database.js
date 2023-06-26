c7c40ea1140c7b5943feb79daa166faeefa4dcd1
Ghisea Stefan <ghiseastefan@gmail.com>
2023-06-26 03:49:18 +0300

    database connect

--- server/config/database.js
+++ server/config/database.js
@@ -4,16 +4,11 @@ mongoose.set('strictQuery', true);
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to the database successfully');
     } catch (err) {
         console.error('Failed to connect to the database:', err);
     }
-};
+}
 
 module.exports = connect;
